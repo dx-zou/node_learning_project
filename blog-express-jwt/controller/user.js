e14a09fbbd8055754f14869274a6bb8243f56e10
zoufeng <casafo@163.com>
2020-01-04 20:23:42 +0800

    密码加密：md5

--- blog-express-jwt/controller/user.js
+++ blog-express-jwt/controller/user.js
@@ -1,26 +1,33 @@
 const { executeSql, escape } = require("../db/mysql");
 const { SuccessModel, ErrorModel } = require("../model/resModel");
-
+const { genPassword } = require("../utils/crypto");
 /**
- * @description 登录接口
+ * @description 用户登录
  * @param {*} req
  * @param {*} res
  * @param {*} next
  * @returns
  */
-const loginCheck = (req, res, next) => {
+const userLogin = (req, res, next) => {
   const data = formCheck(req, res);
   if (!data) return;
   const { username, password } = data;
   const sql = `
     select username, realname,id from users where username=${username} and password=${password}
   `;
+  const sql1 = `select username from users where username=${username}`;
   executeSql(sql).then(result => {
     if (result.length) {
       res.json(new SuccessModel(result[0]));
       return;
     }
-    res.json(new ErrorModel("登录失败"));
+    executeSql(sql1).then(row => {
+      if (!row.length) {
+        res.json(new ErrorModel("用户名不存在"));
+      } else {
+        res.json(new ErrorModel("密码错误"));
+      }
+    });
   });
 };
 
@@ -52,13 +59,16 @@ const userRegister = (req, res, next) => {
  */
 const formCheck = (req, res) => {
   let { username, password } = req.body;
-  username = escape(username);
-  password = escape(password);
   if (!username || !password) {
     res.json(new ErrorModel("用户名或密码不能为空"));
     return false;
   }
+  // 加密密码
+  password = genPassword(password);
+  // 防xss攻击
+  username = escape(username);
+  password = escape(password);
   return { username, password };
 };
 
-module.exports = { loginCheck, userRegister };
+module.exports = { userLogin, userRegister };
--- blog-express-jwt/routes/user.js
+++ blog-express-jwt/routes/user.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { loginCheck, userRegister } = require("../controller/user");
+const { userLogin, userRegister } = require("../controller/user");
 
-router.post("/login", loginCheck);
+router.post("/login", userLogin);
 router.post("/register", userRegister);
 
 module.exports = router;
