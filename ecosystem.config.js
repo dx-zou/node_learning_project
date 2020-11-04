4763e9d0ba8af6a1adc4bb41300da68b749733ff
zoufeng <casafo@163.com>
2020-11-04 13:23:01 +0800

    fix: config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,8 +19,7 @@ module.exports = {
 			path: '/usr/local/www',
 			ssh_options: 'StrictHostKeyChecking=no',
 			'pre-deploy-local': '',
-			'post-deploy':
-				'nvm use v12.19.0 && cnpm install && pm2 reload ecosystem.config.js --env production',
+			'post-deploy': 'pm2 reload ecosystem.config.js --env production',
 			env: {
 				NODE_ENV: 'production',
 			},
