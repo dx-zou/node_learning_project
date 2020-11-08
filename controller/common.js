186ba815cb7d1f8e6b30b57877c460456fb5f023
zoufeng <casafo@163.com>
2020-11-08 20:47:56 +0800

    fix: config

--- controller/common.js
+++ controller/common.js
@@ -1,7 +1,16 @@
 const { SuccessModel, ErrorModel } = global;
+const env = process.env.NODE_ENV;
+const BASE_URL =
+	env === 'development' ? 'http://127.0.0.1:3000' : 'http://47.95.1.121:3000';
+/**
+ * 文件上传
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
 const upload = (req, res, next) => {
 	const filePath = req.file.path.split('public')[1];
-	const fileUrl = `http:127.0.0.1:3000${filePath}`;
+	const fileUrl = `${BASE_URL}${filePath}`;
 	res.json(new SuccessModel(fileUrl));
 };
 
